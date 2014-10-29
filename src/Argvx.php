8b29d90811f5da5f152f766cf9292fd2f1e8fc0a
henderjon <jhenderson@capstonedigital.com>
2014-10-29 13:40:28 -0500

    adds Argvx as a potential replacement for Argv

--- src/Argvx.php
+++ src/Argvx.php
@@ -7,4 +7,102 @@ namespace Chevron\Argv;
  * Someday, I might add strong typing or variations on arg parsing. For now Argv
  * is pretty straightforward.
  */
-// class Argvx {}
+class Argvx {
+
+	/**
+	 * the given argv
+	 */
+	protected $args   = [];
+
+	/**
+	 * an array of values
+	 */
+	protected $values = [];
+
+	/**
+	 * an array of flags
+	 */
+	protected $flags  = [];
+
+	/**
+	 * create a new instance around a given $argv
+	 * @param array $array the argv to parse
+	 * @return Chevron\Argv\Argvx
+	 */
+	function __construct(array $array){
+		$this->args = $array;
+	}
+
+	/**
+	 * set an expected flag value to false and return it via reference for parsing
+	 * @param string $name the name of the flag
+	 * @param string $message an unused value for missing flags
+	 * @return reference
+	 */
+	function &flag($name, $message){
+		$this->flags[$name] = false;
+		return $this->flags[$name];
+	}
+
+	/**
+	 * set an expected value to the default and return it via reference for parsing
+	 * @param string $name the name of the flag
+	 * @param string $message an unused value for missing flags
+	 * @return reference
+	 */
+	function &value($name, $default, $message){
+		$this->values[$name] = $default;
+		return $this->values[$name];
+	}
+
+	/**
+	 * do the parsing
+	 * @return void
+	 */
+	function parse(){
+		$this->parseFlags();
+		$this->parseValues();
+	}
+
+	/**
+	 * parse values from the given argv string
+	 * @param array $values The keys that SHOULD have a value
+	 */
+	protected function parseValues(){
+		$args = $this->args;
+		while( $arg = array_shift($args) ){
+			$arg = trim($arg, " -");
+
+			if( false !== ($pos = strpos($arg, "=")) ){
+				$key                = substr($arg, 0, $pos);
+				$this->values[$key] = substr($arg, ($pos + 1));
+				continue;
+			}
+
+			if( array_key_exists($arg, $this->values) ){
+				$this->values[$arg] = array_shift($args);
+				continue;
+			}
+
+		}
+
+	}
+
+	/**
+	 * parse flags from the given argv string
+	 * @param array $flags The keys that should NOT have a value ... booleans
+	 */
+	protected function parseFlags(){
+		$args = $this->args;
+		while( $arg = array_shift($args) ){
+			$arg = trim($arg, " -");
+
+			if( array_key_exists($arg, $this->flags) ){
+				$this->flags[$arg] = true;
+				continue;
+			}
+		}
+
+	}
+
+}
---
+++ tests/PHPUnit/ArgvxTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Chevron\Argv\Argvx;
+
+class ArgvxTest extends PHPUnit_Framework_TestCase {
+
+	function test_argvx(){
+
+		$args = ["path/to/file", "flag", "value", "this-value"];
+
+		$a = new Argvx($args);
+
+		$flag  =& $a->flag("flag", "This is a flag.");
+		$value =& $a->value("value", "default-value", "This is a value.");
+
+		$nonFlag  =& $a->flag("nonflag", "This is a nonflag.");
+		$nonValue =& $a->value("nonvalue", "default-nonvalue", "This is a value.");
+
+		$this->assertEquals($value, "default-value");
+		$this->assertFalse($flag);
+		$this->assertEquals($nonValue, "default-nonvalue");
+		$this->assertFalse($nonFlag);
+
+		$a->parse();
+
+		$this->assertEquals($value, "this-value");
+		$this->assertTrue($flag);
+		$this->assertEquals($nonValue, "default-nonvalue");
+		$this->assertFalse($nonFlag);
+
+	}
+
+}
